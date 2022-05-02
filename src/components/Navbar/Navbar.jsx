c9aeda82a420830e835f8278335a48c23fed8f82
IvankevichDmitry <dmitry.ivankevich@mail.ru>
2022-05-02 15:10:19 +0300

    10. Navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
+import { NavLink } from "react-router-dom";
 import style from "./Navbar.module.css";
+
 // Можно посмотерть объект style
 // console.log(style);
 
@@ -6,22 +8,22 @@ import style from "./Navbar.module.css";
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}>
-                <a href="/profile">Profile</a>
+            <div>
+                <NavLink to="/profile" className={navData => navData.isActive ? style.active : style.item}>Profile</NavLink>
             </div>
-            <div className={`${style.item} ${style.active}`}>
-                <a href="/dialogs">Messages</a>
+            <div>
+                <NavLink to="/dialogs" className={navData => navData.isActive ? style.active : style.item}>Messages</NavLink>
             </div>
-            <div className={style.item}>
-                <a href="/news">News</a>
+            <div>
+                <NavLink to="/news" className={navData => navData.isActive ? style.active : style.item}>News</NavLink>
             </div>
-            <div className={style.item}>
-                <a href="/music">Music</a>
+            <div>
+                <NavLink to="/music" className={navData => navData.isActive ? style.active : style.item}>Music</NavLink>
             </div>
-            <div className={style.item}>
-                <a href="/settings">Settings</a>
+            <div>
+                <NavLink to="/settings" className={navData => navData.isActive ? style.active : style.item}>Settings</NavLink>
             </div>
-        </nav>
+        </nav >
     )
 }
 
