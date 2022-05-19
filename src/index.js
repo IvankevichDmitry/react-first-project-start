06d6360f1ab975c11dfa377f46ddf78db4767f7d
unknown <dmitry.ivankevich@mail.ru>
2022-05-19 14:44:25 +0300

    Object store

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 
 // Импортируем функцию для постов и сообщений
-import { addMessage, addPost, updateNewMessageText, updateNewPostText } from './redux/state';
+// import state, { subscribe } from './redux/state';
+// import { addMessage, addPost, updateNewMessageText, updateNewPostText } from './redux/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,17 +21,15 @@ let rerenderEntireTree = (state) => {
     <React.StrictMode>
       <BrowserRouter>
           {/* <App posts={posts} dialogs={dialogs} messages={messages}/> */}
-          <App state={state} addPost={addPost} addMessage={addMessage} updateNewPostText={updateNewPostText} updateNewMessageText={updateNewMessageText}/>
+          <App state={state} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}/>
       </BrowserRouter>
     </React.StrictMode>
   );
 };
 
+rerenderEntireTree(store.getState())
 
-
-rerenderEntireTree(state)
-
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,84 +1,95 @@
-// БАЗА ДАННЫХ
+// БАЗА ДАННЫХ 
 
-let rerenderEntireTree = () => {
-    console.log("State")
+let store = {
+    // Личные свойства и методы объекта
+    _state: {
+        // 1. Данные с сообщениями. Используються в MyPosts.jsx
+        profilePage: {
+            posts: [
+                { id: 1, message: "Hi, how are you?", likesCount: 10 },
+                { id: 2, message: "Post 2", likesCount: 30 },
+                { id: 3, message: "Post 3", likesCount: 100 },
+                { id: 4, message: "Post 4", likesCount: 110 },
+            ],
+            newPostText: "Hello"
+        },
+        //  2. Данные с пользователями и сообщениями пользователей. Используються в Dialogs.jsx
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: "Dmitry" },
+                { id: 2, name: "Alina" },
+                { id: 3, name: "Timothy" },
+                { id: 4, name: "Eketerina" },
+                { id: 5, name: "Maxim" },
+                { id: 6, name: "Tanya" },
+            ],
+            messages: [
+                { id: 1, message: "Hi" },
+                { id: 2, message: "Hello" },
+                { id: 3, message: "Good morning" },
+                { id: 4, message: "Day" },
+                { id: 5, message: "Yes" },
+            ],
+            newMessage: "Message"
+        }
+    },
+    _callSubscriber() {
+        console.log("State")
+    },
+
+
+    // Интерфейсные методы объекта для вызова в index.js. Метод 3
+    getState() {
+        return this._state;
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    },
+
+
+    // Все методы 1 и 2
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+    addMessage () {
+        let newMessage = {
+            id: 6,
+            message: this._state.dialogsPage.newMessage,
+        };
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessage = "";
+        this._callSubscriber(this._state)
+    },
+    updateNewMessageText (newMessage) {
+        this._state.dialogsPage.newMessage = newMessage
+        this._callSubscriber(this._state)
+    } 
 }
 
-let state = {
-    // 1. Данные с сообщениями. Используються в MyPosts.jsx
-    profilePage: {
-        posts: [
-            { id: 1, message: "Hi, how are you?", likesCount: 10 },
-            { id: 2, message: "Post 2", likesCount: 30 },
-            { id: 3, message: "Post 3", likesCount: 100 },
-            { id: 4, message: "Post 4", likesCount: 110 },
-        ],
-        newPostText: "Hello"
-    },
-    //  2. Данные с пользователями и сообщениями пользователей. Используються в Dialogs.jsx
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: "Dmitry"},
-            { id: 2, name: "Alina" },
-            { id: 3, name: "Timothy" },
-            { id: 4, name: "Eketerina" },
-            { id: 5, name: "Maxim" },
-            { id: 6, name: "Tanya" },
-        ],
-        messages: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "Hello" },
-            { id: 3, message: "Good morning" },
-            { id: 4, message: "Day" },
-            { id: 5, message: "Yes" },
-        ],
-        newMessage: "Message"
-    }
-};
 
 // 1 СТРАНИЦА POSTS.
 //ФУНКЦИИ ДЛЯ TEXTARE В STATE - INDEX - APP - PROFILE - MYPOSTS
-
-// 1. Функция для создания постов из страницы Profile
-export const addPost = () => {
-     let newPost = {
-         id: 5, 
-         message: state.profilePage.newPostText, 
-         likesCount: 0
-        };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = "";
-    rerenderEntireTree(state)
-};
-
-// 2. Функция изменения текста на ввод сообщений в textarea
-export const updateNewPostText = (newText) => {
-   state.profilePage.newPostText = newText
-   rerenderEntireTree(state)
-};
+// 1. Функция для создания постов из страницы Profile: addPost = () => {}
+// 2. Функция изменения текста на ввод сообщений в textarea: updateNewPostText = (newText) => {}
 
 // 2 СТРАНИЦА DIALOGS. 
 //ФУНКЦИИ ДЛЯ TEXTARE В STATE - INDEX - APP - PROFILE - MYPOSTS
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 6,
-        message: state.dialogsPage.newMessage,
-    };
-   state.dialogsPage.messages.push(newMessage);
-   state.dialogsPage.newMessage = "";
-   rerenderEntireTree(state)
-}
-
-export const updateNewMessageText = (newMessage) => {
-    state.dialogsPage.newMessage = newMessage
-    rerenderEntireTree(state)
-}
+// 1. Функция для создания сообщений из страницы Profile: addMessage = () => {}
+// 2. Функция изменения текста на ввод сообщений в textarea: updateNewMessageText = (newMessage) => {}
 
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
+// 3 ФУНКЦИЯ КОЛБЭК ДЛЯ ОТРИСОВКИ index.js: subscribe = (observer) => {}
 
 
-export default state;
+export default store;
